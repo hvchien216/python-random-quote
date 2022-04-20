0635cdf26b9d90a02ad826107b00f612152dc8d8
Chien Anubis <iocutien2112@gmail.com>
2022-04-20 15:10:50 +0700

    Random quote bot is working

--- get-quote.py
+++ get-quote.py
@@ -1,9 +1,17 @@
+import random
+
+last = 13
+rnd = random.randint(0, last)
+
+print(rnd)
+
 def primary():
   f = open("quotes.txt")
   quotes = f.readlines()
   f.close()
 
   print(quotes[len(quotes)-1])
+  print(rnd)
 
 if __name__== "__main__":
   primary()
